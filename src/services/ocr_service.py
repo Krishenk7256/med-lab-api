16e8aaa9f2293364bf9ac0d7449c6eddd3bb1683
koks7256 <maksim7256@gmail.com>
2026-05-24 13:21:49 +0500

    Feat: Added decorators for repeating a function (Sync/Async) and changing its execution speed

--- src/services/ocr_service.py
+++ src/services/ocr_service.py
@@ -5,48 +5,51 @@ from pdf2image import convert_from_bytes
 import pytesseract
 from fastapi.concurrency import run_in_threadpool
 
+from src.utils.decorators import retry, log_sync_time, log_async_time
 
-class OCRService:
-    # Поддержка пдф, изображений, таблиц. Это всё синхронно, но заворачивается в тредпул
-    @staticmethod
-    def _parse_pdf(file_bytes: bytes) -> str:
-        images = convert_from_bytes(file_bytes)
-        full_text = []
-        for image in images:
-            text = pytesseract.image_to_string(image, lang="rus+eng")
-            full_text.append(text)
-        return "".join(full_text)
-
-    @staticmethod
-    def _parse_image(file_bytes: bytes) -> str:
-        image = Image.open(io.BytesIO(file_bytes))
+# Поддержка пдф, изображений, таблиц. Это всё синхронно, но заворачивается в тредпул
+#@log_sync_time
+def parse_pdf(file_bytes: bytes) -> str:
+    images = convert_from_bytes(file_bytes)
+    full_text = []
+    for image in images:
         text = pytesseract.image_to_string(image, lang="rus+eng")
-        return text.strip()
+        full_text.append(text)
+    return "".join(full_text)
 
+#@log_sync_time
+def parse_image(file_bytes: bytes) -> str:
+    image = Image.open(io.BytesIO(file_bytes))
+    text = pytesseract.image_to_string(image, lang="rus+eng")
+    return text.strip()
+
+#@log_sync_time
+def parse_table(file_bytes: bytes, is_csv = False) -> str:
+    buffer = io.BytesIO(file_bytes)
+    df = pd.read_csv(buffer) if is_csv else pd.read_excel(buffer)
+    return df.to_string(index = False)
+
+# --- КЛАСС-ФАСАД ДЛЯ РОУТЕРА ---
+class OCRService:
     @staticmethod
-    def _parse_table(file_bytes: bytes, is_csv = False) -> str:
-        buffer = io.BytesIO(file_bytes)
-
-        df = pd.read_csv(buffer) if is_csv else pd.read_excel(buffer)
-
-        return df.to_string(index = False)
-
-
-    @staticmethod
+    @retry()
+    @log_async_time
     async def extract_text(file_bytes: bytes, content_type: str) -> str:
         try:
             match content_type:
                 case "application/pdf":
-                    return await run_in_threadpool(OCRService._parse_pdf, file_bytes)
+                    return await run_in_threadpool(parse_pdf, file_bytes)
                 case "image/png" | "image/webp" | "image/jpeg":
-                    return await run_in_threadpool(OCRService._parse_image, file_bytes)
+                    return await run_in_threadpool(parse_image, file_bytes)
                 case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" | "application/vnd.ms-excel":
-                    return await run_in_threadpool(OCRService._parse_table, file_bytes, is_csv = False)
+                    return await run_in_threadpool(parse_table, file_bytes, is_csv = False)
                 case "text/csv":
-                    return await run_in_threadpool(OCRService._parse_table, file_bytes, is_csv = True)
+                    return await run_in_threadpool(parse_table, file_bytes, is_csv = True)
                 case _:
                     return f"[Ошибка]: Неподдерживаемый формат файла: {content_type}"
         except Exception as e:
             return f"[Ошибка обработки файла {content_type}]: {str(e)}"
 
 ocr_service = OCRService()
+
+
---
+++ src/utils/decorators.py
@@ -0,0 +1,52 @@
+import time
+import logging
+import asyncio
+import functools
+
+logger = logging.getLogger("app_logger")
+
+# До лучших времён
+# def log_async_time(func):
+#     """
+#     Декоратор, который замеряет время выполнения Async функции.
+#     """
+#     @functools.wraps(func)
+#     async def wrapper(*args, **kwargs):
+#         start_time = time.perf_counter()
+#         result = await func(*args, **kwargs)
+#         execution_time = time.perf_counter() - start_time
+#         logger.info(f"Async функция [{func.__name__}] выполнена за {execution_time:.4f} сек.")
+#         return result
+#     return wrapper
+
+def log_sync_time(func):
+    """
+    Декораторый, который замеряет время выполнения Sync функции.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        execution_time = time.perf_counter() - start_time
+        logger.info(f"Sync функция [{func.__name__}] выполнена за {execution_time:.4f} сек.")
+
+def retry(retries: int = 3, delay: float = 1.0):
+    """
+    Декоратор с параметрами.
+    Повторяет выполнение async функции при падении.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            attempt = 0
+            while attempt < retries:
+                try:
+                    return await func(*args, **kwargs)
+                except Exception as e:
+                    attempt += 1
+                    logger.warning(f"Ошибка в {func.__name__} (Попытка {attempt}/{retries}): {e}")
+                    if attempt >= retries:
+                        raise e
+                    await asyncio.sleep(delay)
+        return wrapper
+    return decorator
