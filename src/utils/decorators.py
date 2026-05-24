9446394d5c38f60cb060ac20785fe47d8afb5954
koks7256 <maksim7256@gmail.com>
2026-05-24 14:32:40 +0500

    Fix: Commented out the required decorator

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -5,30 +5,30 @@ import functools
 
 logger = logging.getLogger("app_logger")
 
-# До лучших времён
-# def log_async_time(func):
-#     """
-#     Декоратор, который замеряет время выполнения Async функции.
-#     """
-#     @functools.wraps(func)
-#     async def wrapper(*args, **kwargs):
-#         start_time = time.perf_counter()
-#         result = await func(*args, **kwargs)
-#         execution_time = time.perf_counter() - start_time
-#         logger.info(f"Async функция [{func.__name__}] выполнена за {execution_time:.4f} сек.")
-#         return result
-#     return wrapper
-
-def log_sync_time(func):
+def log_async_time(func):
     """
-    Декораторый, который замеряет время выполнения Sync функции.
+    Декоратор, который замеряет время выполнения Async функции.
     """
     @functools.wraps(func)
-    def wrapper(*args, **kwargs):
+    async def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
-        result = func(*args, **kwargs)
+        result = await func(*args, **kwargs)
         execution_time = time.perf_counter() - start_time
-        logger.info(f"Sync функция [{func.__name__}] выполнена за {execution_time:.4f} сек.")
+        logger.info(f"Async функция [{func.__name__}] выполнена за {execution_time:.4f} сек.")
+        return result
+    return wrapper
+
+# До лучших времён
+# def log_sync_time(func):
+#     """
+#     Декораторый, который замеряет время выполнения Sync функции.
+#     """
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         start_time = time.perf_counter()
+#         result = func(*args, **kwargs)
+#         execution_time = time.perf_counter() - start_time
+#         logger.info(f"Sync функция [{func.__name__}] выполнена за {execution_time:.4f} сек.")
 
 def retry(retries: int = 3, delay: float = 1.0):
     """
